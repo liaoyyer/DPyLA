92aa5ffd662b5801b7f027d379a3485be825af8c
liaoyyer <liaoyyer@gmail.com>
2015-11-14 22:31:39 +0000

    Merge pull request #3 from fitnr/master
    :+1: These changes look great.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'dpla',
@@ -8,8 +8,9 @@ setup(
     author = 'Chad Nelson',
     author_email = 'chadbnelson@gmail.com',
     url = 'https://github.com/bibliotechy/DPyLA',   # use the URL to the github repo
-    download_url = 'https://github.com/bibliotechy/DPyLA/tarball/0.2', # I'll explain this in a second
+    download_url = 'https://github.com/bibliotechy/DPyLA/archive/0.2.tar.gz',
     keywords = ['libraries', 'DPLA', 'museums'], # arbitrary keywords
+    test_suite='tests',
     classifiers = [],
     install_requires = [ 'requests' ]
     )
